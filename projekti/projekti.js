58a9867033fe4cebd91d3a4532a8ed4bf1a9139e
rot315 <47969738+rot315@users.noreply.github.com>
2019-05-04 14:10:33 +0300

    Add files via upload

--- projekti/projekti.js
+++ projekti/projekti.js
@@ -7,3 +7,60 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
       'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   id: 'mapbox.streets'
 }).addTo(mymap);
+
+
+var x = document.getElementById("demo");
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  }
+  missa = prompt('sijainti');
+  aika = prompt('pysäköintiaika');
+  varoitus = prompt('pysäköinti rajoituksia')
+ }
+
+function showPosition(position) {
+  let la =  position.coords.latitude;
+  let lo =  position.coords.longitude;
+  let marker = L.marker([la, lo]).addTo(mymap)
+    .bindPopup(missa+" "+ aika +" - "+aika1).openPopup();
+
+}
+
+function Gako(){
+  alert("Gako");
+}
+
+let coord = e.latlng;
+let lat = coord.lat;
+let lng = coord.lng;
+let marker = L.marker([lat, lng]).
+    addTo(mymap).bindPopup("Sijainti: " + mis + "Pysäköinti aika: " + ai + "Rajoitukset: " + varo).
+    openPopup();
+
+
+function onMapClick(e) {
+  mis = prompt('sijainti');
+  ai = prompt('pysäköintiaika');
+  varo = prompt('pysäköinti rajoituksia');
+}
+
+
+function jusus() {
+
+    mymap.on('click', onMapClick);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
